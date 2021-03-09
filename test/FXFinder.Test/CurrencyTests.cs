4ab0b96baf5b8ea1cd76f24ab919473ec02dd9d6
alexovonathan22 <avo.nathan@gmail.com>
2021-03-09 16:36:56 +0100

    Small Change unit ts

--- test/FXFinder.Test/CurrencyTests.cs
+++ test/FXFinder.Test/CurrencyTests.cs
@@ -62,7 +62,7 @@ namespace FXFinder.Test
 
             };
             var currMgr = new Mock<ICurrencyManager>();
-            currMgr.Setup(t => t.ChangeMainCurrency("kub", model.UserId)).ReturnsAsync((cur, string.Empty));
+            currMgr.Setup(t => t.ChangeMainCurrency("kpb", model.UserId)).ReturnsAsync((cur, string.Empty));
             var _sut = new CurrencyController(currMgr.Object);
             //Act
             var output = await _sut.ChangeUserCurrency(model);
