a8fd57e1966429d903f24ed89b271ef79c31aa64
alexovonathan22 <avo.nathan@gmail.com>
2021-03-22 14:47:16 +0100

    changed azure reources for api

--- src/FXFinder.Core/Migrations/20210322134301_Changed azure-conn string.Designer.cs
+++ src/FXFinder.Core/Migrations/20210322134301_Changed azure-conn string.Designer.cs
@@ -4,14 +4,16 @@ using FXFinder.Core.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FXFinder.Core.Migrations
 {
     [DbContext(typeof(WalletDbContext))]
-    partial class WalletDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210322134301_Changed azure-conn string")]
+    partial class Changedazureconnstring
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
