d00ed9cb813907ae74f6e5d213140f6f77858da5
alexovonathan22 <avo.nathan@gmail.com>
2021-03-22 09:58:30 +0100

    more of house cleaning

--- src/FXFinder.API/Startup.cs
+++ src/FXFinder.API/Startup.cs
@@ -143,10 +143,6 @@ namespace FXFinder.API
             // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //var context = services.GetService<WalletDbContext>();
-            //context.Database.Migrate();
-
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
